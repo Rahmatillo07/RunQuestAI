dd93dd24bf15c149c7202008a6be7b33fa069632
Rahmatillo <Rahmatillo@gmail.com>
2025-11-16 15:03:01 +0600

    errors damn

--- RunQuestAi/settings.py
+++ RunQuestAi/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # STATIC & MEDIA
 # ========================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []  # optional
 
 MEDIA_URL = '/media/'
--- RunQuestAi/urls.py
+++ RunQuestAi/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,6 +29,6 @@ urlpatterns = [
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
 
     path('accounts/', include('allauth.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
